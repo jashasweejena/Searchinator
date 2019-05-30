b518ac060a81b033b118acd42d76aa46c576fc75
Jashaswee Jena <j.ishujena2000@gmail.com>
2019-05-30 16:09:37 +0530

    searchinator: Fix indentation

--- searchinator.py
+++ searchinator.py
@@ -15,16 +15,16 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
 
 def getFileContent(pathAndFileName):
-    with open(pathAndFileName, 'r') as theFile:
-        # Return a list of lines (strings)
-        # data = theFile.read().split('\n')
+        with open(pathAndFileName, 'r') as theFile:
+                # Return a list of lines (strings)
+                # data = theFile.read().split('\n')
 
-        # Return as string without line breaks
-        # data = theFile.read().replace('\n', '')
+                # Return as string without line breaks
+                # data = theFile.read().replace('\n', '')
 
-        # Return as string
-        data = theFile.read()
-        return data
+                # Return as string
+                data = theFile.read()
+                return data
 
 # api_key = getFileContent({}/'telegram_api_key.txt'.format(dir_path))
 
@@ -33,51 +33,52 @@ updater = Updater('')
 dispatcher = updater.dispatcher
 
 def getdata(que):
-    url="https://eztv.io/search/" + que.replace(" ", "-")
-    print(url)
-    page=requests.get(url)
-    d = dict()
-    soup=BeautifulSoup(page.text,'html.parser')
+        url="https://eztv.io/search/" + que.replace(" ", "-")
+        print(url)
+        page=requests.get(url)
+        d = dict()
+        soup=BeautifulSoup(page.text,'html.parser')
 
-    links=[]
-    titles=[]
+        links=[]
+        titles=[]
 
-    namel=soup.select("a[class='magnet']")
-    for link in namel:
-        links.append(link.get('href'))
-        titles.append(link.get('title'))
-    d['titles']=titles
-    d['links']=links
+        namel=soup.select("a[class='magnet']")
+        for link in namel:
+                links.append(link.get('href'))
+                titles.append(link.get('title'))
+        d['titles']=titles
+        d['links']=links
 
-    json_string = json.dumps(d)
-    return d
+        json_string = json.dumps(d)
+        return d
 
 
 
 def start_callback(bot, update, args):
 #     update.message.reply_text("Welcome to my awesome bot!")
-    user_says = " ".join(args)
-    print(user_says)
-    update.message.reply_text("You said: " + user_says)
+        user_says = " ".join(args)
+        print(user_says)
+        update.message.reply_text("You said: " + user_says)
 
 dispatcher.add_handler(CommandHandler("start", start_callback, pass_args=True))
 
 def fetch(bot, update, args):
-    text_fetch = ' '.join(args)
-    d = getdata(text_fetch)
-    bot.send_message(chat_id=update.message.chat_id, text=d['titles'][0])
-    bot.send_message(chat_id=update.message.chat_id, text=d['links'][0])
-    print(d['titles'], end='\n')
-   # print(d['links'], end='\n \n')
+        text_fetch = ' '.join(args)
+        d = getdata(text_fetch)
+        bot.send_message(chat_id=update.message.chat_id, text=d['titles'][0])
+        bot.send_message(chat_id=update.message.chat_id, text=d['links'][0])
+        print(d['titles'], end='\n')
+        # print(d['links'], end='\n \n')
 
 caps_handler = CommandHandler('fetch', fetch, pass_args=True)
 dispatcher.add_handler(caps_handler)
 
 def forward(update, context):
-    message = update.message['reply_to_message']['text']
-    print(message)
-    user_says = " ".join(context.args)
-    update.message.reply_text("You said: " + user_says)
+        # do something
+        message = update.message['reply_to_message']['text']
+        print(message)
+        user_says = " ".join(context.args)
+        update.message.reply_text("You said: " + user_says)
 
 dispatcher.add_handler(CommandHandler("forward", forward, pass_args=True))
 
